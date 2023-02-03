291ae40ffe823fcccea0b6887a1526393d4a25fc
kr1s7ian <kristianboy44@gmail.com>
2023-02-03 07:25:39 +0100

    removed Suspense and react lazy, modified GridCanvas to use props

--- src/pages/editor_page.tsx
+++ src/pages/editor_page.tsx
@@ -1,12 +1,19 @@
-import React, { Suspense } from "react";
-const LazyGridCanvas = React.lazy(() => import("../components/grid_canvas"));
+import GridCanvas from "../components/grid_canvas";
+import { createGridContext, useGrid } from "../hooks/useGrid";
 
 export const EditorPage = () => {
+  const grid = useGrid(createGridContext(150, 150, false, 1));
   return (
     <div className="editor">
-      <Suspense>
-        <LazyGridCanvas />
-      </Suspense>
+      <button onClick={grid.randomizeCells}>randomize cells</button>
+      <input
+        type="range"
+        value={grid.ctx.cellSize}
+        max={500}
+        min={1}
+        onChange={(event) => grid.setCellSize(Number(event.target.value))}
+      />
+      <GridCanvas grid={grid} />
     </div>
   );
 };

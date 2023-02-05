fb66c212769a066d06dc841e403b9d89d903f82a
kr1s7ian <kristianboy44@gmail.com>
2023-02-05 15:12:33 +0100

    restructured

--- src/pages/editor_page.tsx
+++ src/pages/editor_page.tsx
@@ -2,17 +2,11 @@ import GridCanvas from "../components/grid_canvas";
 import { createGridContext, useGrid } from "../hooks/useGrid";
 
 export const EditorPage = () => {
-  const grid = useGrid(createGridContext(150, 150, false, 1));
+  const grid = useGrid(createGridContext(250, 250, false, 1));
   return (
     <div className="editor">
       <button onClick={grid.randomizeCells}>randomize cells</button>
-      <input
-        type="range"
-        value={grid.ctx.cellSize}
-        max={500}
-        min={1}
-        onChange={(event) => grid.setCellSize(Number(event.target.value))}
-      />
+      <button onClick={() => grid.fillCells(false)}>clear cells</button>
       <GridCanvas grid={grid} />
     </div>
   );

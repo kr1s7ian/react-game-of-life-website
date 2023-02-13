3843eef0a214efc74c837d84f64d9f64d73bbc2c
kr1s7ian <kristianboy44@gmail.com>
2023-02-13 17:19:49 +0100

    fixed missed / on base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "react-game-of-life-website",
+  base: "/react-game-of-life-website",
   plugins: [react()],
   test: {
     environment: "happy-dom",

8fddd45cd15ee38018e105afcb4c45ec079dde48
kr1s7ian <kristianboy44@gmail.com>
2023-02-13 17:10:05 +0100

    added base

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 /// <reference types="vitest" />
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "react-game-of-life-website",
   plugins: [react()],
   test: {
     environment: "happy-dom",
   },
-})
+});

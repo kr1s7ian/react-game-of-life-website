67a8966daca32c2a4084f1b5003f001f11fa3e5d
kr1s7ian <kristianboy44@gmail.com>
2023-02-13 16:43:01 +0100

    added navbar to homepage

--- src/pages/home_page.tsx
+++ src/pages/home_page.tsx
@@ -1,7 +1,10 @@
+import { Navbar } from "../components/navbar";
+
 export const HomePage = () => {
   return (
     <div className="homepage">
       <h1>HOME PAGE</h1>
+      <Navbar></Navbar>
     </div>
   );
 };
